70081a7e28aff1de58012da643272ec38f33d9e7
Raphael <raph.rych@gmail.com>
2021-05-30 14:38:29 -0400

    adicionado ex 010, 011

---
+++ aulas/aula011/ex08.js
@@ -0,0 +1,6 @@
+var vel = 60.1
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+if(vel >= 60){
+    console.log('VOCÊ ULTRAPASSOU A VELOCIDADE ERMITIDA')
+    console.log('PORTANTO FOI MULTADO!')
+}
---
+++ aulas/aula011/ex09.js
@@ -0,0 +1,7 @@
+var país = 'brasil'.toUpperCase()
+console.log(`Vivendo em ${país}.`)
+if (país == 'BRASIL'){
+    console.log('Por tanto você é Brasileiro!')
+}else{
+    console.log('Por tanto você é Estrenjeiro!')
+}

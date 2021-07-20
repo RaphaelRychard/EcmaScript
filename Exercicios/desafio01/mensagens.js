f6beb13007f09f1833b78355d0fe78230181c79f
Raphael <raph.rych@gmail.com>
2021-07-20 18:31:09 -0400

    adicionado exercicios

---
+++ Exercicios/desafio01/mensagens.js
@@ -0,0 +1,9 @@
+function clicarBotao1(){
+    alert("Voce clicou no 1° Botão!")
+}
+function clicarBotao2(){
+    alert("Você Clicou no 2° Botão!")
+}
+function clicarBotao3(){
+    alert("Você clicou no 3° Botão!")
+}
---
+++ Exercicios/desafio02/app.js
@@ -0,0 +1,5 @@
+function InicieOdesafio(){
+    var nome = prompt("Qual é seu nome? ")
+    var idade = prompt(`Olá ${nome}!, Quantos anos você tem? `)
+    alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}

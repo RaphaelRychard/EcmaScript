47aa31d9ff6d6690a44509cf50c78fcc1c7a5440
Raphael <raph.rych@gmail.com>
2021-07-20 23:52:59 -0400

    add exercicios 03, 04

---
+++ Exercicios/desafio03/app.js
@@ -0,0 +1,6 @@
+function Verificar(){
+    numero = parseInt(prompt("Digite um numero inteiro qualquer? "))
+    sucessor = numero + 1
+    antecessor = numero - 1
+    alert(`Antes do ${numero}, temos o número ${antecessor}\nDepois do ${numero}, temos o número ${sucessor}`)
+}
---
+++ Exercicios/desafio04/app.js
@@ -0,0 +1,7 @@
+function comprar(){
+    produto = String(prompt('Que protudo você está comprando? '))
+    preço = Number(prompt(`Quanto custa o ${produto} que você está comprando? `)).toFixed(2)
+    valor = Number(prompt(`Qual foi o valor que deu para pagar o ${produto}`)).toFixed(2)
+    conta = (valor - preço).toFixed(2)
+    alert(`Você comprou o ${produto} que custou ${preço}\n Deu R$ ${valor} em dinheiro e vai receber R$ ${conta} de troco!`)
+}

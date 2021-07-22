983db29e2b608c9754f27a91959fe21c13ec3710
Raphael <raph.rych@gmail.com>
2021-07-21 23:57:52 -0400

    exercicios do desafio 07 finalizado

--- Exercicios/desafio06/app.js
+++ Exercicios/desafio06/app.js
@@ -1,8 +1,10 @@
-function conversor () {
-    var celsios = Number(prompt('Difite uma temperrratura em °C (Celsius)'))
+function conversor() {
+    var celsios = parseFloat(prompt('Difite uma temperrratura em °C (Celsius)'))
     var resusltado = document.querySelector('article.resultado')
 
-    resusltado.innerHTML =  `<h2>A temperatura de ${celsios}°C, corresponde a...</h2>`
-    var CalculoFahrenhait = Number((celsios * 9/5) + 32)
-    resusltado.innerHTML += `${CalculoFahrenhait} K (Fahrenhait)`
+    resusltado.innerHTML = `<h2>A temperatura de ${celsios}°C, corresponde a...</h2>`
+    var CalculoKelvin = parseFloat(celsios + 273.15)
+    var CalculoFahrenhait = parseFloat((celsios * 9 / 5) + 32)
+    resusltado.innerHTML += `<p>${CalculoKelvin.toFixed(2).replace('.', ',')} °K (Kelvin)</p>`
+    resusltado.innerHTML += `<p>${CalculoFahrenhait.toFixed(2).replace('.', ',')} °F (Fahrenhait)</p>`
 }
---
+++ Exercicios/desafio07/app.js
@@ -0,0 +1,12 @@
+var doalReal = Number(prompt('Antes de mais nada quantos está do doloar agora? '.replace('.', ',')))
+
+function converter () {
+
+    var resultado = document.querySelector('article.resultado')
+    var SobraReal = Number(prompt('Quantos R$ tem na carteira? '))
+
+    var Covdolar = SobraReal / doalReal
+    
+    resultado.innerHTML += `<p>O seu dinheiro em Dolar sera de: ${Covdolar.toLocaleString('pt-br', {style:'currency', currency: 'USD'} )}</p>`
+
+}

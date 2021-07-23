5f968b3663514ec6a1ef2c01df06c52791e88cc0
Raphael <raph.rych@gmail.com>
2021-07-23 18:59:49 -0400

    Exericioa 012 em andamento

---
+++ Exercicios/desafio12/app.js
@@ -0,0 +1,6 @@
+function verificar() {
+
+    var precoAntigo = Number(prompt('Qaul era o preço anterior do produto? '))
+    var precoNovo = Number(prompt('Qual é o preço atual do produto? '))
+    
+}
